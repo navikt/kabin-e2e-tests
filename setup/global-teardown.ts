47f90ba623cdb1237ab79243e2e8f8cef2eacd0e
Christian Skrøvseth <3177301+cskrov@users.noreply.github.com>
2026-09-18 10:28:12 +0200

    Stop feilregistrering of behandlinger before delete

--- setup/global-teardown.ts
+++ setup/global-teardown.ts
@@ -1,7 +1,7 @@
-import { feilregistrerKabalBehandlinger } from '@/setup/feilregistrer-and-delete';
+import { deleteKabalBehandlinger } from '@/setup/delete-kabal-behandlinger';
 
 export default async () => {
   console.debug('Running global teardown tasks...');
 
-  await feilregistrerKabalBehandlinger();
+  await deleteKabalBehandlinger('global_teardown');
 };
